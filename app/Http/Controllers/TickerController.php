e772543b4fe2056c195f214956bdf53da885307d
sthembisoo <sthembisotshabalala26@gmail.com>
2022-09-12 09:24:01 +0200

    updates

--- app/Http/Controllers/TickerController.php
+++ app/Http/Controllers/TickerController.php
@@ -10,6 +10,7 @@ class TickerController extends BaseController
 
 public function index()
 {
+   //service call to get the spreadsheet data
     $values =  MyServiceProvider::getStockData();
      return view("index", ["stock"=>$values]);
 
@@ -17,6 +18,7 @@ public function index()
 
 public function refresh()
 {
+   //get updated spreadsheet data
    return  $this->index();
 }
 
--- app/Providers/MyServiceProvider.php
+++ app/Providers/MyServiceProvider.php
@@ -7,11 +7,8 @@ use Revolution\Google\Sheets\Facades\Sheets;
 
 class MyServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     *
-     */
+    //service to get the spreadsheet data
+    //initially stored the spreadsheet id's in the config
     public static function getStockData()
     {
         return Sheets::spreadsheet('11Prx4AB7_krtTwIhrNSiq_ZQeOLCwWwJm-SeE2WaUw8')->sheetById('0')->all();
